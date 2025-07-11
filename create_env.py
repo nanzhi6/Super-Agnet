77d77e17447af3455a16a632792bdd8ce6ba495f
nanzhi6 <134184993+nanzhi6@users.noreply.github.com>
2025-07-11 08:50:02 +0800

    Update create_env.py

--- create_env.py
+++ create_env.py
@@ -3,7 +3,7 @@ def create_env_file():
 OPENAI_API_KEY=your_openai_api_key_here
 OPENAI_BASE_URL=https://api.openai.com/v1
 # deepseekAPI配置
-deepseek_API_KEY = 'sk-4e63e2e8dd6f4d54a0dfc6a05d565325'
+deepseek_API_KEY = your_deepseek_api_key_here
 
 # 历史信息文件路径
 STOCK_HISTORY_FILE=data/stock_history.json
